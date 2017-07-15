dbc2d066566336d308b19b926916f89ea617975d
Logan Darklock <logandarklock@gmail.com>
2017-07-15 12:55:09 -0700

    Add shebang and comment to start.sh file

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+
+# start.sh file. run this file when you want to build and start initbuntu.
+
 docker build -t 'logandark/initbuntu:latest' .
 docker run --tmpfs /tmp --tmpfs /run -v /sys/fs/cgroup:/sys/fs/cgroup:ro -it --rm logandark/initbuntu
